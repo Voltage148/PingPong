aca19ba086c93bfb3b0e94292d326f6580d6fa7c
Voltage <poyaVoltage148@gmail.com>
2023-04-06 11:30:30 +0430

    Changes:Add NeededMEthods.h && Write main method

---
+++ SRC/NeededMethods.h
@@ -0,0 +1,10 @@
+/*
+    This file has the importent methods for program:
+*/
+
+#ifndef NeededMethods_H   
+#define NeededMethods_H
+
+
+
+#endif
---
+++ SRC/PingMain.cpp
@@ -0,0 +1,34 @@
+/*
+    The Start of Main File:
+        This file has Life Loop and this file controls the game cycle
+        The main method set the default vars.
+*/
+
+//Start:
+
+//Import the headers:
+#include "NeededMethods.h"
+using namespace std;
+
+//Start of main method:
+int main(int argc, char** arvg)
+{
+    //The Code:
+
+    //Define the base vars:
+    int PlayerHP = 3;   int Score = 0;  int Time = 0;   int PlayersMovement = 0;    bool Game = true; //Game is the importent var in the program; 
+
+    //Define the Objects:
+
+    
+    
+//==================================Start Main Game======================
+
+    //Showing the ground and set the window:
+    
+
+    //Main Loop:
+
+
+    return 0;
+}
