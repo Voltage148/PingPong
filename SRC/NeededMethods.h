b394401840dbd97300fe1426abf72986084a4d38
Voltage <poyaVoltage148@gmail.com>
2023-04-06 13:28:48 +0430

    Changes:Complete the NeededMethods.h

--- SRC/NeededMethods.h
+++ SRC/NeededMethods.h
@@ -1,10 +1,73 @@
 /*
-    This file has the importent methods for program:
+    This file has the important methods for program:
 */
 
 #ifndef NeededMethods_H   
 #define NeededMethods_H
 
+//Import Needed Headers:
+#include <windows.h>
+#include <winuser.h>
+#include <conio.h>
+
+using namespace std;
+
+
+HANDLE Console = GetStdHandle(STD_OUTPUT_HANDLE);
+COORD Cursor;
+
+namespace Methods
+{
+
+    void FullScreen(bool);
+    
+    void SetCursor(int, int);
+
+    void SetTextColor(int);
+
+    void HideCursor(bool);
+};
+
+void Methods::FullScreen(bool flag = true)
+{
+    if(flag == true)
+    {
+        ShowWindow(GetConsoleWindow(),SW_MAXIMIZE);
+        SendMessage(GetConsoleWindow(),WM_SYSKEYDOWN,VK_RETURN,0x20000000);
+    }
+    else
+    {
+        SendMessage(GetConsoleWindow(), WM_SYSKEYDOWN, VK_RETURN, 0x20000000);
+        system("mode con COLS=200");
+    }
+    return;
+}
+
+void Methods::SetCursor(int x = 0, int y = 0)
+{
+    Cursor.Y = y;
+    Cursor.X = x;
+    SetConsoleCursorPosition(Console, Cursor);
+    return;
+}
+
+void Methods::SetTextColor(int Color_Number = 2)
+{
+    SetConsoleTextAttribute(Console, Color_Number);
+    return;
+}
+
+void Methods::HideCursor(bool flag = true)
+{
+
+    CONSOLE_CURSOR_INFO         CursorInfo;
+    HANDLE                      Data = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    GetConsoleCursorInfo(Data, &CursorInfo);
+    CursorInfo.bVisible = flag;
+    SetConsoleCursorInfo(Data, &CursorInfo);
+    return;
+}
 
 
 #endif
--- SRC/PingMain.cpp
+++ SRC/PingMain.cpp
@@ -7,6 +7,10 @@
 //Start:
 
 //Import the headers:
+#include <windows.h>
+#include <winuser.h>
+
+
 #include "NeededMethods.h"
 using namespace std;
 
@@ -16,7 +20,7 @@ int main(int argc, char** arvg)
     //The Code:
 
     //Define the base vars:
-    int PlayerHP = 3;   int Score = 0;  int Time = 0;   int PlayersMovement = 0;    bool Game = true; //Game is the importent var in the program; 
+    int PlayerHP = 3;   int Score = 0;  int Time = 0;   int PlayersMovement = 0;    bool Game = true; //Game is the important var in the program;
 
     //Define the Objects:
 
@@ -25,7 +29,15 @@ int main(int argc, char** arvg)
 //==================================Start Main Game======================
 
     //Showing the ground and set the window:
-    
+
+    //Window Setup:
+    Methods::FullScreen(true);                                      Sleep(12);
+    ShowScrollBar(GetConsoleWindow(), SB_VERT, 0);     Sleep(12);
+    Methods::HideCursor(false);
+
+    //PlayGround:
+
+
 
     //Main Loop:
 
